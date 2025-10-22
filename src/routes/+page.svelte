28f2e4e14de57b8884d92225cbe9d986399d5975
tofilagman <tofilagman@gmail.com>
2025-10-22 22:43:25 +0800

    create project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+    import { goto } from '$app/navigation';
+
+    const create = () => {
+        goto('/create');
+    }
+
+    const open = () => {
+        goto('/designer');
+    }
+</script>
 <div class="w-full">
     <div class="m-auto mt-80 bg-white w-200 border rounded-md shadow-lg"> 
         <div class="grid grid-cols-[auto_20rem] gap-4 w-full h-100">
@@ -5,8 +16,8 @@
                <p class="text-center text-4xl">Kim Photobooth</p>
             </div>
             <div class="my-40 flex flex-col gap-4">
-                <a>Create New</a>
-                <a>Open</a>
+                <a on:click={create()} href="javascript:void(0);">Create New</a>
+                <a on:click={open()} href="javascript:void(0);">Open</a>
             </div>
         </div>
     </div>
