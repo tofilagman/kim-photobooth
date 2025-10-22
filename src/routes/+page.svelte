47288b8f31bec8db50f4dc30dd346924b225c6f5
tofilagman <tofilagman@gmail.com>
2025-10-22 21:10:42 +0800

    initial project files

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="w-full">
+    <div class="m-auto mt-80 bg-white w-200 border rounded-md shadow-lg"> 
+        <div class="grid grid-cols-[auto_20rem] gap-4 w-full h-100">
+            <div class="m-4 bg-[url($lib/assets/images/bg-main.jpg)] bg-cover">
+               <p class="text-center text-4xl">Kim Photobooth</p>
+            </div>
+            <div class="my-40 flex flex-col gap-4">
+                <a>Create New</a>
+                <a>Open</a>
+            </div>
+        </div>
+    </div>
+</div>
